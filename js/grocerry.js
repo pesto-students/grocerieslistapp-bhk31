b38463de580d14016e2ae1a7ac4a3d3235552eb5
bhk31 <bhkhandelwal92@gmail.com>
2020-12-17 23:49:11 +0530

    Committing ne UI

--- js/grocerry.js
+++ js/grocerry.js
@@ -1,130 +1,171 @@
-let user = {
-    canAddItem: true,
-};
+var h = window.innerHeight;
+
+document.querySelector("body").setAttribute("style", "height:" + h);
 
 const LIMIT = 5;
+let user = {
+  canAddItem: true,
+  items: [],
+};
 
 addItemToItems = addListItem();
 
-document.getElementsByClassName('deleteButton').onclick = function() {
-console.log(this.id);
-}
+getSelectedElement("login").onclick = function () {
+  user.userName = getInputElementFromForm("login-form", "user-name");
 
-// document.getElementById('add-item').onclick = function() {
+  if (user.userName) {
+    users = JSON.parse(localStorage.getItem("users"));
 
-//     if( user.canAddItem === true) {
-//         addItemToItems();
-//     } else {
-//         document.getElementById('error-message').classList.remove('hide');
-//     }
+    if (users && users[user.userName]) {
+      user = users[user.userName];
+      let strHtml = "";
 
-// }
+      if (user.limit !== 5) {
+        user.items.forEach((element) => {
+          strHtml = strHtml + createListItem(element);
+        });
+      }
 
-document.getElementById('add-user-name').onclick = function() {
-
-user.userName = document.getElementById('user-name').value;
-
-if(localStorage.length != 0) {
-    if(typeof localStorage.getItem(user.userName) == "string") {
-        user = JSON.parse(localStorage.getItem(user.userName));
+      document
+        .getElementById("listContainer")
+        .insertAdjacentHTML("beforeend", strHtml);
     }
+
+    getSelectedElement("loggedIn").classList.remove("hide");
+    getSelectedElement("loginContainer").classList.add("hide");
+    getSelectedElement("heading").innerHTML =
+      "Welcome " + user.userName + " to your grocery list";
+
+    if (false === user.canAddItem) {
+      warningAndDisableButton();
+    }
+
+    getSelectedElement("limit").innerHTML = LIMIT - user.items.length;
+  } else {
+    getSelectedElement("required-field-user").classList.remove("hide");
+  }
+};
+
+function createListItem(element) {
+  return (
+    '<div class="list-item"><div class="item-name">' +
+    element +
+    '</div><div class="button-container"><button id="delete-' +
+    element +
+    '"onclick="deleteItem(\'' +
+    element +
+    '\')">Delete</button><button id="edit-' +
+    element +
+    '" onclick="editItem(\'' +
+    element +
+    "')\">Edit</button></div></div>"
+  );
 }
 
-if(user.limit !== 5) {
-    user.items.forEach(element => {
-        createListItem(element);
+getSelectedElement("save").onclick = function () {
+  if (true === user.canAddItem) {
+    addItemToItems();
+  }
 
-    });
-}
-
-if(false === user.canAddItem) {
-    warningAndDisableButton();
-}
-
-document.getElementById('user-names').classList.add('hide')
-document.getElementById('user-items').classList.remove("hide");
-document.getElementById('message').innerHTML = 'Welcome ' + user.userName + ' to your Grocery list';
-document.getElementById('limit').innerHTML =  LIMIT - user.items.length;
-}
+  save();
+};
 
 function addListItem() {
-user.items = [];
-let count = 0;
+  return function addToArray() {
+    let formData = new FormData(getSelectedElement("grocerry-form"));
+    let item = formData.get("item");
 
-return function addToArray() {
-    let item = document.getElementById('item').value;
-    
-    createListItem(item);
-    
-    user.items.push(item);
-    
-    user.limit = LIMIT - user.items.length;
-    document.getElementById('limit').innerHTML = user.limit
-    
-    if( 5 === user.items.length) {
-        user.canAddItem = false;
-        warningAndDisableButton();
+    if (item) {
+      addHtmlClass("required-field", "hide");
+
+      if (false === compareListItems(item)) {
+        addHtmlClass("existing-item", "hide");
+
+        let strHtml = createListItem(item);
+
+        document
+          .getElementById("listContainer")
+          .insertAdjacentHTML("beforeend", strHtml);
+
+        user.items.push(item);
+
+        user.limit = LIMIT - user.items.length;
+        getSelectedElement("limit").innerHTML = user.limit;
+
+        if (5 === user.items.length) {
+          user.canAddItem = false;
+          warningAndDisableButton();
+        }
+        console.log(user);
+      } else {
+        getSelectedElement("existing-item").classList.remove("hide");
+      }
+    } else {
+      getSelectedElement("required-field").classList.remove("hide");
     }
-    console.log(user);
-}
-}
-
-function save() {
-localStorage.setItem(user.userName, JSON.stringify(user));
-console.log(localStorage.getItem(user.userName));
+  };
 }
 
-
-document.getElementById('save').onclick = function() {
-
-if(true === user.canAddItem ) {
-    addItemToItems()
-}
-
-save();
+function compareListItems(item) {
+  return user.items.includes(item);
 }
 
 function warningAndDisableButton() {
-document.getElementById('warning-message').classList.remove('hide');
+  getSelectedElement("warning-message").classList.remove("hide");
 }
 
-function createListItem(element) {
-let listItem = document.createElement("li");
-listItem.setAttribute('data-element', element)
+function save() {
+  let users = {};
 
-let listText = document.createTextNode(element);
+  if (localStorage.getItem("users")) {
+    users = JSON.parse(localStorage.getItem("users"));
+  }
 
-let btn =  document.createElement("BUTTON");
-let buttonText = document.createTextNode('Delete')
-btn.appendChild(buttonText);
-btn.setAttribute('class', 'deleteButton');
-btn.setAttribute('id', element);
-btn.setAttribute('onclick', 'deleteItem(\''+element+'\')');
+  let key = user.userName;
+  users[key] = user;
 
-listItem.appendChild(listText);
-listItem.append(btn);
+  console.log(JSON.stringify(users));
+  localStorage.setItem("users", JSON.stringify(users));
+  users = JSON.parse(localStorage.getItem("users"));
+  keys = Object.keys(users);
+  let usersCount = keys.length;
 
-document.getElementById('list').appendChild(listItem);
+  if (4 === usersCount) {
+    deleteAndUpdateUsers(users, keys, usersCount);
+  }
 }
 
-document.getElementById('add-new-user').onclick = function() {
-location.reload();
+function deleteAndUpdateUsers(users, keys, count) {
+  delete users[keys[0]];
+  localStorage.setItem("users", JSON.stringify(users));
 }
 
 function deleteItem(element) {
+  user.items = user.items.filter(function (item) {
+    return item !== element;
+  });
 
-user.items = user.items.filter(function(item) {
-    return item !== element
-});
+  if (5 > user.items.length) {
+    user.canAddItem = true;
+    user.limit = LIMIT - user.items.length;
+    addHtmlClass("warning-message", "hide");
+  }
 
-if(5 > user.items.length) {
-    user.canAddItem =true;
-    user.limit = LIMIT-user.items.length;
-    document.getElementById('warning-message').classList.add('hide');
+  getSelectedElement("delete-" + element).parentNode.parentNode.remove();
+  getSelectedElement("limit").innerHTML = user.limit;
+
+  save();
 }
 
-document.getElementById(element).parentNode.remove()
-document.getElementById('limit').innerHTML = user.limit;
+function getSelectedElement(element) {
+  return document.getElementById(element);
+}
 
-save();        
+function getInputElementFromForm(form, input) {
+  let formData = new FormData(getSelectedElement(form));
+  return formData.get(input);
+}
+
+function addHtmlClass(element, className) {
+  getSelectedElement(element).classList.add(className);
 }
